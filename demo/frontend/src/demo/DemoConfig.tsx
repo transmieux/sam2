b5f35c3a4d983e48648b891e57f9d7292211288e
Chirag Vaghasiya <chiragvaghasiya444@gmail.com>
2024-11-07 17:47:46 +0530

    Added .env file

--- demo/frontend/src/demo/DemoConfig.tsx
+++ demo/frontend/src/demo/DemoConfig.tsx
@@ -20,6 +20,8 @@ type EffectLayers = {
   highlight: keyof Effects;
 };
 
+const API_URL = import.meta.env.APP_URL;
+
 export const DEMO_SHORT_NAME = 'SAM 2 Demo';
 export const RESEARCH_BY_META_AI = 'By Meta FAIR';
 export const DEMO_FRIENDLY_NAME = 'Segment Anything 2 Demo';
@@ -31,8 +33,8 @@ export const ABOUT_URL = 'https://ai.meta.com/sam2';
 export const EMAIL_ADDRESS = 'segment-anything@meta.com';
 export const BLOG_URL = 'http://ai.meta.com/blog/sam2';
 
-export const VIDEO_API_ENDPOINT = 'http://localhost:7263';
-export const INFERENCE_API_ENDPOINT = 'http://localhost:7263';
+export const VIDEO_API_ENDPOINT = `${API_URL}`;
+export const INFERENCE_API_ENDPOINT = `${API_URL}`;
 
 export const demoObjectLimit = 3;
 
